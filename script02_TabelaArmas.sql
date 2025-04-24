6f9ebdde2cfa6ad2f30487a63df5a7c5cde70bde
Gabrielle Vasconcelos <gabriellevasconcelosoliveira@gmail.com>
2025-04-23 22:09:51 -0300

    DS-AULA09

---
+++ script02_TabelaArmas.sql
@@ -0,0 +1,28 @@
+BEGIN TRANSACTION;
+CREATE TABLE [TB_ARMAS] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] varchar(200) NOT NULL,
+    [Dano] int NOT NULL,
+    CONSTRAINT [PK_TB_ARMAS] PRIMARY KEY ([Id])
+);
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Dano', N'Nome') AND [object_id] = OBJECT_ID(N'[TB_ARMAS]'))
+    SET IDENTITY_INSERT [TB_ARMAS] ON;
+INSERT INTO [TB_ARMAS] ([Id], [Dano], [Nome])
+VALUES (1, 35, 'Adaga'),
+(2, 33, 'Espada'),
+(3, 36, 'Machado'),
+(4, 50, 'Onyx Viper'),
+(5, 34, 'Rifle'),
+(6, 33, 'Foice'),
+(7, 32, 'Orbe de Distorção');
+
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Dano', N'Nome') AND [object_id] = OBJECT_ID(N'[TB_ARMAS]'))
+    SET IDENTITY_INSERT [TB_ARMAS] OFF;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250416202215_MigracaoArma', N'9.0.3');
+
+COMMIT;
+GO
